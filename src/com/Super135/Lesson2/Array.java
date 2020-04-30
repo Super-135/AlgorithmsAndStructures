4e1cd181b688794216811b54fea1cdbef22b4a80
Super-135 <super05061982@gmail.com>
2020-04-30 14:00:02 +0300

    Lesson2

---
+++ src/com/Super135/Lesson2/Array.java
@@ -0,0 +1,40 @@
+package com.Super135.Lesson2;
+
+public interface Array<E> {
+
+    int INITIAL_CAPACITY = 4;
+
+    void add(E value);
+
+    E get(int index);
+
+    boolean remove(E value);
+
+    E remove(int index);
+
+    default boolean contains(E value) {
+        return indexOf(value) != -1;
+    }
+
+    int indexOf(E value);
+
+    int size();
+
+    default boolean isEmpty(){
+        return size() == 0;
+    }
+
+
+    default void display(){
+        System.out.print("[");
+        for (int i = 0; i <size() ; i++) {
+            E value = get(i);
+            System.out.print(value + ", ");
+        }
+        System.out.println("}");
+    }
+
+
+
+
+}
